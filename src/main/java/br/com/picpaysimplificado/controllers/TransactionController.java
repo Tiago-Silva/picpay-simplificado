2ea8598c59822acfea96994e22720c3abdca73e8
Tiago <ti.almeida58@yahoo.com>
2023-08-23 10:55:12 -0300

    feat: Create Transaction controller

---
+++ src/main/java/br/com/picpaysimplificado/controllers/TransactionController.java
@@ -0,0 +1,25 @@
+package br.com.picpaysimplificado.controllers;
+
+import br.com.picpaysimplificado.domain.transaction.Transaction;
+import br.com.picpaysimplificado.dtos.TransactionDTO;
+import br.com.picpaysimplificado.services.TransactionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController()
+@RequestMapping("/transactions")
+public class TransactionController {
+
+    @Autowired
+    private TransactionService transactionService;
+
+    @PostMapping
+    public ResponseEntity<Transaction> createTransaction(@RequestBody TransactionDTO transactionDTO) throws Exception {
+        Transaction newTransaction = this.transactionService.createTransaction(transactionDTO);
+        return new ResponseEntity<>(newTransaction, HttpStatus.CREATED);
+    }
+}
