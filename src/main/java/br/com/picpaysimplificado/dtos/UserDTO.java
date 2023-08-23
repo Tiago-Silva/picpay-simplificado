47f7f5e979f631ea8db2df619eeb0f1b67c8216d
Tiago <ti.almeida58@yahoo.com>
2023-08-23 10:51:59 -0300

    feat: Create user DTO

---
+++ src/main/java/br/com/picpaysimplificado/dtos/UserDTO.java
@@ -0,0 +1,10 @@
+package br.com.picpaysimplificado.dtos;
+
+import br.com.picpaysimplificado.domain.user.UserType;
+
+import java.math.BigDecimal;
+
+public record UserDTO(String firstName, String lastName, String document,
+                      BigDecimal balance, String email,
+                      String password, UserType userType) {
+}
